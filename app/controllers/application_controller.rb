b92d7b049429e650147a7e734e54cde625e337cf
SeiyaTanio <basket.cp3.spc@icloud.com>
2021-11-23 12:26:55 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configere_permitted_paarameters, if: :devise_controller?
+
+  private
+  def configere_permitted_paarameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
